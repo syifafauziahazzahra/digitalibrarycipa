aee0555d2c82089c612cb03ca2b6e36f9158bcb4
syifafauziahazzahra <syifafauziahazzahra01@gmail.com>
2024-01-10 13:45:12 +0700

    Initial commit

---
+++ database/migrations/2024_01_10_020200_create_bukus_table.php
@@ -0,0 +1,26 @@
+<?php
+// database/migrations/xxxx_xx_xx_create_bukus_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBukusTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id('bukuID');
+            $table->string('judul');
+            $table->string('penulis');
+            $table->string('penerbit');
+            $table->integer('tahun_terbit');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+}
